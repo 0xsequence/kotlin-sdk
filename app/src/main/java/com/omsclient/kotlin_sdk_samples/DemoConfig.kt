f474209e2e9b1fcd99bd2f35952330df8aa66449
Tolgahan <tolgahan.arikan@gmail.com>
2026-07-02 17:16:57 +0300

    Update example publishable key

--- app/src/main/java/com/omsclient/kotlin_sdk_samples/DemoConfig.kt
+++ app/src/main/java/com/omsclient/kotlin_sdk_samples/DemoConfig.kt
@@ -1,7 +1,7 @@
 package com.omsclient.kotlin_sdk_samples
 
 internal object DemoConfig {
-    const val demoPublishableKey: String = "pk_dev_sdbx_01kqa06hyyetj_01kv5zt5s3eke9038q8y67jdvj"
+    const val demoPublishableKey: String = "pk_sdbx_01kqfw9zaykks_01kwetq606fv699qb9bhfmb45s"
     const val demoGoogleWebClientId: String =
         "913882656162-7l4ofa0ou2hqo90umlkenhdop1f5inba.apps.googleusercontent.com"
     const val googleIssuer: String = "https://accounts.google.com"
--- trails-actions/src/main/java/com/omsclient/kotlin_sdk_trails_actions/TrailsActionsActivity.kt
+++ trails-actions/src/main/java/com/omsclient/kotlin_sdk_trails_actions/TrailsActionsActivity.kt
@@ -2149,7 +2149,7 @@ class TrailsActionsActivity : AppCompatActivity() {
 }
 
 private object DemoConfig {
-    const val demoPublishableKey: String = "pk_dev_sdbx_project_key"
+    const val demoPublishableKey: String = "pk_sdbx_01kqfw9zaykks_01kwetq606fv699qb9bhfmb45s"
     const val demoGoogleWebClientId: String = "913882656162-7l4ofa0ou2hqo90umlkenhdop1f5inba.apps.googleusercontent.com"
     const val oidcRedirectUri: String = "omsclientkotlindemo://auth/callback"
 }
