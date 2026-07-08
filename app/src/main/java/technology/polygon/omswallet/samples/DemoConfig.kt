698d5c42b3014751fbb19d17428b4b644db04814
Tolgahan <tolgahan.arikan@gmail.com>
2026-07-08 17:05:42 +0300

    feat: add Google ID token sample flow

--- app/src/main/java/technology/polygon/omswallet/samples/DemoConfig.kt
+++ app/src/main/java/technology/polygon/omswallet/samples/DemoConfig.kt
@@ -3,7 +3,7 @@ package technology.polygon.omswallet.samples
 internal object DemoConfig {
     const val demoPublishableKey: String = "pk_sdbx_01kqfw9zaykks_01kwetq606fv699qb9bhfmb45s"
     const val demoGoogleWebClientId: String =
-        "913882656162-7l4ofa0ou2hqo90umlkenhdop1f5inba.apps.googleusercontent.com"
+        "970987756660-0dh5gubqfiugm452raf7mm39qaq639hn.apps.googleusercontent.com"
     const val googleIssuer: String = "https://accounts.google.com"
     const val oidcRedirectUri: String = "omsclientkotlindemo://auth/callback"
 }
